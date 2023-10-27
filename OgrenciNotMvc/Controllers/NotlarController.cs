7ab32ff0e263422a607ec4e7f3fa593d58070094
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-28 00:17:33 +0300

    NotlarController #1

---
+++ OgrenciNotMvc/Controllers/NotlarController.cs
@@ -0,0 +1,20 @@
+﻿using OgrenciNotMvc.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OgrenciNotMvc.Controllers
+{
+    public class NotlarController : Controller
+    {
+		DbMvcOkulEntities db = new DbMvcOkulEntities();
+		// GET: Notlar
+		public ActionResult Index()
+        {
+			var notlar = db.TblNotlar.ToList();
+            return View(notlar);
+        }
+    }
+}
