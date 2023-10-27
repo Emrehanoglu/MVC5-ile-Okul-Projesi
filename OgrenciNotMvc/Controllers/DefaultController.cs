a7ed99c41eb1f6cdfdb15f840d5e159854e6144c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-27 22:49:30 +0300

    DefaultController #1

--- OgrenciNotMvc/Controllers/DefaultController.cs
+++ OgrenciNotMvc/Controllers/DefaultController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OgrenciNotMvc.Models.EntityFramework;
 
 namespace OgrenciNotMvc.Controllers
 {
     public class DefaultController : Controller
     {
+		DbMvcOkulEntities db = new DbMvcOkulEntities();
         // GET: Default
         public ActionResult Index()
         {
-            return View();
+			var dersler = db.TblDersler.ToList();
+            return View(dersler);
         }
     }
 }
