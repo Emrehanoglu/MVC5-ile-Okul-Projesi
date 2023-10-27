5e4a665e53357f7310a8c0f7749d94fad29e950b
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-28 00:06:19 +0300

    OgrencilerController #1

---
+++ OgrenciNotMvc/Controllers/OgrencilerController.cs
@@ -0,0 +1,20 @@
+﻿using OgrenciNotMvc.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OgrenciNotMvc.Controllers
+{
+    public class OgrencilerController : Controller
+    {
+		DbMvcOkulEntities db = new DbMvcOkulEntities();
+		// GET: Ogrenciler
+		public ActionResult Index()
+        {
+			var ogrenciler = db.TblOgrenciler.ToList();
+            return View(ogrenciler);
+        }
+    }
+}
