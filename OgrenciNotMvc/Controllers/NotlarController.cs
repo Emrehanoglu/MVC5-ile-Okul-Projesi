32de7b593e62f6c5506375f98da1b9cbcbe2644b
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-29 23:41:01 +0300

    NotlarController #3

--- OgrenciNotMvc/Controllers/NotlarController.cs
+++ OgrenciNotMvc/Controllers/NotlarController.cs
@@ -19,11 +19,27 @@ namespace OgrenciNotMvc.Controllers
 		[HttpGet]
 		public ActionResult NotEkle()
 		{
+			List<SelectListItem> ogrenci = (from x in db.TblOgrenciler
+											select new SelectListItem
+											{
+												Text = x.Ad + " " + x.Soyad,
+												Value = x.Id.ToString()
+											}).ToList();
+			ViewBag.ogr = ogrenci;
+			List<SelectListItem> ders = (from y in db.TblDersler
+											select new SelectListItem
+											{
+												Text = y.DersAd,
+												Value = y.DersId.ToString()
+											}).ToList();
+			ViewBag.drs = ders;
 			return View();
 		}
 		[HttpPost]
 		public ActionResult NotEkle(TblNotlar not)
 		{
+			db.TblNotlar.Add(not);
+			db.SaveChanges();
 			return RedirectToAction("Index");
 		}
 	}
